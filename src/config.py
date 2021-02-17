ec4b21cc961ab56c8cb40e69e3939e999e5b5621
robin <robin.song@edmonton.ca>
2021-02-17 07:59:09 -0700

    update for zerobox

--- src/config.py
+++ src/config.py
@@ -16,21 +16,32 @@ import os
 root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 port = 4101
-gpus = '-1'
-master_address = 'http://127.0.0.1:8817'
-router_address = 'http://127.0.0.1:9001'
+gpus = "-1"
 
-face_config = dict(modelname='face_retrieval.face',
-                   model_path=os.path.join(root_path, 'model', '20180402-114759'))
+# change: to use environment varaibles other than hard coding
+# this change is better for running in docker
+vearch_host = os.environ.get("VEARCH_HOST", "127.0.0.1")
+master_port = os.environ.get("MASTER_PORT", "8817")
+router_port = os.environ.get("ROUTER_PORT", "9001")
+master_address = f"http://{vearch_host}:{master_port}"
+router_address = f"http://{vearch_host}:{router_port}"
 
-image_config = dict(modelname='image_retrieval.image_extract.vgg16',
-                    detectname=None)
+face_config = dict(
+    modelname="face_retrieval.face",
+    model_path=os.path.join(root_path, "model", "20180402-114759"),
+)
 
-video = dict(db='video',
-             space='video',
-             ip='http://127.0.0.1',
-             imagepath=os.path.join(root_path, 'images', 'face_retrieval'),
-             videopath='rtmp://58.200.131.2:1935/livetv/hunantv')
+image_config = dict(modelname="image_retrieval.image_extract.vgg16", detectname=None)
 
-text = dict(modelname='text.text',
-            model_path=os.path.join(root_path, 'model', 'chinese_L-12_H-768_A-12'))
+video = dict(
+    db="video",
+    space="video",
+    ip="http://127.0.0.1",
+    imagepath=os.path.join(root_path, "images", "face_retrieval"),
+    videopath="rtmp://58.200.131.2:1935/livetv/hunantv",
+)
+
+text = dict(
+    modelname="text.text",
+    model_path=os.path.join(root_path, "model", "chinese_L-12_H-768_A-12"),
+)
